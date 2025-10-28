1176512326aab59ba7ca212f9bd0eb8a9bda3f6c
Christian Byars <cbyars9785@sdsu.edu>
2025-10-27 21:39:42 -0700

    intial commit

---
+++ src/SQL/createDB.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+
+database = "activity.db"
+
+#initial table for primary functionality
+tables = [
+    """CREATE TABLE IF NOT EXISTS activity_sessions (
+            session_id INTEGER PRIMARY KEY,
+            start_time TIME NOT NULL,
+            end_time TIME NOT NULL,
+            app TEXT NOT NULL,
+            window_title TEXT NOT NULL,
+            url TEXT,
+            category TEXT
+            );"""
+]
+
+try:
+    with sqlite3.connect(database) as conn:
+        cursor = conn.cursor()
+        
+        for table in tables:
+            cursor.execute(table)
+        conn.commit()
+        print("Tables created")
+        
+except sqlite3.OperationalError as e:
+    print("Failed to create database:", e)
