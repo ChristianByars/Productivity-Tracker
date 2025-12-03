88b584c7e5488eced6a41f514384e012d55b5e0c
Christian <cbyars9785@sdsu.edu>
2025-12-03 08:19:06 -0800

    Modularized code and tracked google chrome tabs

--- src/backend/activity.py
+++ src/backend/activity.py
@@ -1,37 +1,35 @@
 import sqlite3
 import pywin, win32gui, win32process
 import psutil
-import time
 
 def get_window():
     try:
-        current_window = win32gui.GetForegroundWindow()
-
-        """give your system time to obtain the next window. If you don't sleep,
-            your computer will not be able to accurately track your windows"""
-        time.sleep(2)
-
-        _,pid = win32process.GetWindowThreadProcessId(current_window)
+        hwnd = win32gui.GetForegroundWindow()
+        _, pid = win32process.GetWindowThreadProcessId(hwnd)
         process = psutil.Process(pid)
-        process_name = process.name()  
-        return current_window, process_name
-        
+        process_name = process.name()
+        return hwnd, process_name
+    
     except Exception:
         return None, None
 
 def store_window():
-    prev_window = None
-    prev_tab, tab_name = None, None
+    """These variables prevent program from constantly recording same tab
+        and/or application"""
+    prev_window, prev_tab = None, None
+
     while True:
         #get current window/application being used
-        tab, current_window = get_window()
-        tab_name = win32gui.GetWindowText(tab)
+        hwnd, current_window = get_window()
         
-        if tab_name and tab_name != prev_tab:
-            #tab_name = win32gui.GetWindowText(tab)
-            print(f'Chrome window: {tab_name}')
-            prev_tab = tab_name
+        #get google chrome tab
+        if current_window == "chrome.exe" and hwnd:
+            tab_name = win32gui.GetWindowText(hwnd)
+            if tab_name != prev_tab:
+                print(f'Chrome window: {tab_name}')
+                prev_tab = tab_name
 
+        #record current window
         if current_window and current_window != prev_window:
             print(f'Current Applcation: {current_window}')    
             prev_window = current_window
